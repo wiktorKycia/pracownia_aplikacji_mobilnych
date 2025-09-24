9e0955ea5936e6e3dcc7b0bc83c9e507840d7084
wiktor <wiktor.kycia@gmail.com>
2025-09-24 12:10:02 +0200

    do the 4.2 task

--- 2025_09_24/javaProject/src/main/java/org/example/Main.java
+++ 2025_09_24/javaProject/src/main/java/org/example/Main.java
@@ -62,6 +62,8 @@ public class Main
     {
         File file = new File("./Dane_2205/liczby.txt");
 
+
+        // reading the file length
         int file_length = 0;
         try (Scanner lineCounter = new Scanner(file)) {
             while (lineCounter.hasNextLine()) {
@@ -73,6 +75,8 @@ public class Main
             e.printStackTrace();
         }
 
+
+        // reading a whole file to the array
         String[] numbers = new String[file_length];
 
         try (Scanner reader = new Scanner(file))
@@ -89,19 +93,34 @@ public class Main
             e.printStackTrace();
         }
 
+
+        // doing the things
         ArrayList<String> valid_numbers = new ArrayList<String>();
+        int number_with_the_most_primes = 0;
+        int number_with_the_most_different_primes = 0;
 
         for(int i = 0; i < numbers.length; i++)
         {
+            // checking condition for 4.1
             if(numbers[i].charAt(0) == numbers[i].charAt(numbers[i].length()-1))
             {
                 valid_numbers.add(numbers[i]);
             }
+
+            // calculations for 4.2
+            if(czynniki_pierwsze(number_with_the_most_primes) < czynniki_pierwsze(Integer.parseInt(numbers[i])))
+            {
+                number_with_the_most_primes = Integer.parseInt(numbers[i]);
+            }
+            if(rozne_czynniki_pierwsze(number_with_the_most_different_primes) < rozne_czynniki_pierwsze(Integer.parseInt(numbers[i])))
+            {
+                number_with_the_most_different_primes = Integer.parseInt(numbers[i]);
+            }
         }
 
-        try {
+        try { // saving 4.1 to a file
             FileWriter writer = new FileWriter("wyniki4.txt");
-            String text = valid_numbers.getFirst() + " " + valid_numbers.toArray().length;
+            String text = valid_numbers.getFirst() + " " + valid_numbers.toArray().length + "\n";
             writer.write(text);
             writer.close();
         } catch (IOException e) {
@@ -109,7 +128,21 @@ public class Main
             e.printStackTrace();
         }
 
-        System.out.println(czynniki_pierwsze(420));
-        System.out.println(rozne_czynniki_pierwsze(420));
+        try { // saving 4.2 to a file
+            FileWriter writer = new FileWriter("wyniki4.txt");
+            String text = number_with_the_most_primes + " " + czynniki_pierwsze(number_with_the_most_primes) + "\n" +
+                    number_with_the_most_different_primes + " " + rozne_czynniki_pierwsze(number_with_the_most_different_primes);
+            writer.write(text);
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+
+            // testing the methods
+//        System.out.println(czynniki_pierwsze(420));
+//        System.out.println(rozne_czynniki_pierwsze(420));
+
     }
 }
