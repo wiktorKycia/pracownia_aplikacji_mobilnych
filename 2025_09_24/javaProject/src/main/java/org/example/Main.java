d1521a418567e069922efc32759f823e0555c9da
wiktor <wiktor.kycia@gmail.com>
2025-09-24 11:41:10 +0200

    finish 4.1 task

--- 2025_09_24/javaProject/src/main/java/org/example/Main.java
+++ 2025_09_24/javaProject/src/main/java/org/example/Main.java
@@ -3,6 +3,9 @@ package org.example;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main
 {
@@ -10,16 +13,49 @@ public class Main
     {
         File file = new File("./Dane_2205/liczby.txt");
 
+        int file_length = 0;
+        try (Scanner lineCounter = new Scanner(file)) {
+            while (lineCounter.hasNextLine()) {
+                lineCounter.nextLine();
+                file_length++;
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        String[] numbers = new String[file_length];
+
         try (Scanner reader = new Scanner(file))
         {
-            while(reader.hasNextLine())
+            for(int i = 0;reader.hasNextLine(); i++)
             {
                 String data = reader.nextLine();
-                System.out.println(data);
+                numbers[i] = data;
             }
         }
         catch (FileNotFoundException e)
         {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        ArrayList<String> valid_numbers = new ArrayList<String>();
+
+        for(int i = 0; i < numbers.length; i++)
+        {
+            if(numbers[i].charAt(0) == numbers[i].charAt(numbers[i].length()-1))
+            {
+                valid_numbers.add(numbers[i]);
+            }
+        }
+
+        try {
+            FileWriter writer = new FileWriter("wyniki4.txt");
+            String text = valid_numbers.getFirst() + " " + valid_numbers.toArray().length;
+            writer.write(text);
+            writer.close();
+        } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
