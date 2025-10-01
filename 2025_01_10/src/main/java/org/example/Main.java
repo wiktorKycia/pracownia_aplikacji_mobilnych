9745889216496358deb268a862256bccd709d1f5
wiktor <wiktor.kycia@gmail.com>
2025-10-01 11:15:54 +0200

    add some functions

--- 2025_01_10/src/main/java/org/example/Main.java
+++ 2025_01_10/src/main/java/org/example/Main.java
@@ -1,14 +1,50 @@
 package org.example;
 
+import java.util.Random;
+import java.util.Scanner;;
 
-public class Main {
-    static void main() {
+public class Main
+{
+    static Random rand = new Random();
+    static void main()
+    {
 
-        IO.println(String.format("Hello and welcome!"));
+        IO.println("Hello and welcome!");
+        int number = getNumberOfDices();
+        System.out.println(number);
 
-        for (int i = 1; i <= 5; i++) {
-
-            IO.println("i = " + i);
+    }
+    static int getNumberOfDices()
+    {
+        Scanner scanner = new Scanner(System.in);
+        while(true)
+        {
+            System.out.println("Ile kostek chcesz rzucić(3-10)");
+            String input = scanner.nextLine();
+            try
+            {
+                return Integer.parseInt(input);
+            }
+            catch (java.lang.NumberFormatException _) {}
+        }
+    }
+    static int getRandomNumber(int a, int b)
+    {
+        return rand.nextInt(a, b+1);
+    }
+    static int[] generateRandomDices(int numberOfDices)
+    {
+        int[] dices = new int[numberOfDices];
+        for(int i = 0; i < numberOfDices; i++)
+        {
+            dices[i] = getRandomNumber(1, 6);
+        }
+        return dices;
+    }
+    static void printDices(int[] dices)
+    {
+        for (int i = 0; i < dices.length; i++) {
+            System.out.println("Kostka "+i+1+": "+dices[i]);
         }
     }
 }
