1588d10f8df92df808f0f733d1546ad27d4031c2
wiktor <wiktor.kycia@gmail.com>
2025-12-03 10:12:49 +0100

    add button onclick function

--- 2025_12_03/app/src/main/java/com/example/inf04_22/MainActivity.java
+++ 2025_12_03/app/src/main/java/com/example/inf04_22/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.inf04_22;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +24,34 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText emailEntry = (EditText) findViewById(R.id.editTextText);
+                String email = emailEntry.getText().toString();
+                TextView output = (TextView) findViewById(R.id.outputTextView);
+                output.setText("Autor: 00000000000");
+                if(!email.contains("@"))
+                {
+                    output.setText("Autor: 00000000000\n"+MainActivity.this.getString(R.string.to_nie_jest_email));
+                }
+                else
+                {
+                    EditText passwordEntry1 = (EditText) findViewById(R.id.editTextTextPassword);
+                    EditText passwordEntry2 = (EditText) findViewById(R.id.editTextTextPassword2);
+
+                    if(!passwordEntry1.getText().toString().equals(passwordEntry2.getText().toString()))
+                    {
+                        output.setText("Autor: 00000000000\n"+MainActivity.this.getString(R.string.hasla_sie_nie_zgadzaja));
+                    }
+                    else
+                    {
+                        output.setText("Autor: 00000000000\nWitaj " + email);
+                    }
+                }
+            }
         });
     }
 }
