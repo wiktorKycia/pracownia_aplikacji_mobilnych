b73d56b22e4093e7cc0073a6295575c73dd4aa5f
wiktor <wiktor.kycia@gmail.com>
2026-01-16 18:34:01 +0100

    make code more readable

--- 2026_01_07/app/src/main/java/com/example/inf04_22/MainActivity.java
+++ 2026_01_07/app/src/main/java/com/example/inf04_22/MainActivity.java
@@ -13,22 +13,27 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
         Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
                 EditText emailEntry = (EditText) findViewById(R.id.editTextText);
                 String email = emailEntry.getText().toString();
                 TextView output = (TextView) findViewById(R.id.outputTextView);
