0f28d5979e3d70f8d916eeed3eda6f7d258dde4f
wiktor <wiktor.kycia@gmail.com>
2026-01-16 18:42:02 +0100

    style the button better

--- 2026_01_07/app/src/main/java/com/example/inf04_22/MainActivity2.java
+++ 2026_01_07/app/src/main/java/com/example/inf04_22/MainActivity2.java
@@ -15,10 +15,12 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
 }
