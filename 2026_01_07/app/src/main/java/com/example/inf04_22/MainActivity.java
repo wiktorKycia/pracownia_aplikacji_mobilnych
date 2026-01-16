a7346848dc6a705483d70188c37b2f8e1d6508ac
wiktor <wiktor.kycia@gmail.com>
2026-01-16 20:16:57 +0100

    correct the not-working regex

--- 2026_01_07/app/src/main/java/com/example/inf04_22/MainActivity.java
+++ 2026_01_07/app/src/main/java/com/example/inf04_22/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.inf04_22;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -49,7 +50,7 @@ public class MainActivity extends AppCompatActivity
                 String password = passwordEntry1.getText().toString();
 
                 // Email
-                if(!Pattern.matches("^(a-z0-9.)+@(a-z0-9.)+$", email)){
+                if(!Pattern.matches("^[a-zA-Z0-9.-_]+@[a-zA-Z0-9.-_]+$", email)){
                     output.setText("Autor: 00000000000\n"+MainActivity.this.getString(R.string.to_nie_jest_email));
                 }
                 else if (!password.equals(passwordEntry2.getText().toString()))
