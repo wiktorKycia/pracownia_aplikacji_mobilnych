813ca0f01323dd5d1ae0dbe003bc8307111d9bfb
wiktor <wiktor.kycia@gmail.com>
2026-02-07 12:57:15 +0100

    add first fragment

--- 2026_02_04/app/src/main/java/com/example/a2026_02_04/MainActivity.java
+++ 2026_02_04/app/src/main/java/com/example/a2026_02_04/MainActivity.java
@@ -28,24 +28,21 @@ public class MainActivity extends AppCompatActivity {
         });
 
         Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText emailEntry = (EditText) findViewById(R.id.editTextEmail);
-                String email = emailEntry.getText().toString();
+        button.setOnClickListener(v -> {
+            EditText emailEntry = (EditText) findViewById(R.id.editTextEmail);
+            String email = emailEntry.getText().toString();
 
-                if(Pattern.matches("^[a-zA-Z0-9.-_]+@[a-zA-Z0-9.-_]+$", email))
+            if(Pattern.matches("^[a-zA-Z0-9.-_]+@[a-zA-Z0-9.-_]+$", email))
+            {
+                EditText nameEntry = findViewById(R.id.editTextText2);
+                String name = nameEntry.getText().toString();
+
+                EditText surnameEntry = findViewById(R.id.editTextText3);
+                String surname = nameEntry.getText().toString();
+
+                if (name.equals("Ryszard") && surname.equals("Pyssa"))
                 {
-                    EditText nameEntry = findViewById(R.id.editTextText2);
-                    String name = nameEntry.getText().toString();
 
-                    EditText surnameEntry = findViewById(R.id.editTextText3);
-                    String surname = nameEntry.getText().toString();
-
-                    if (name.equals("Ryszard") && surname.equals("Pyssa"))
-                    {
-
-                    }
                 }
             }
         });
