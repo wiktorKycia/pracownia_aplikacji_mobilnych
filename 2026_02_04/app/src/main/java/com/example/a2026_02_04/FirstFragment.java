b6056a73aa7d580faf778cea4dbc24c5d64c7e1b
wiktor <wiktor.kycia@gmail.com>
2026-02-07 13:29:35 +0100

    move the logic to the Fragment's onCreateView

--- 2026_02_04/app/src/main/java/com/example/a2026_02_04/FirstFragment.java
+++ 2026_02_04/app/src/main/java/com/example/a2026_02_04/FirstFragment.java
@@ -3,26 +3,53 @@ package com.example.a2026_02_04;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- */
+import java.util.regex.Pattern;
+
 public class FirstFragment extends Fragment {
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+        Button button = view.findViewById(R.id.button);
+        button.setOnClickListener(v -> {
+            FragmentActivity mainActivity = getActivity();
+            assert mainActivity != null;
+            EditText emailEntry = (EditText) view.findViewById(R.id.editTextEmail);
+            String email = emailEntry.getText().toString();
 
-//        binding = FragmentFirstBinding.inflate(inflater, container, false);
-//        return binding.getRoot();
-        return inflater.inflate(R.layout.fragment_first, container, false);
+            if(Pattern.matches("^[a-zA-Z0-9.-_]+@[a-zA-Z0-9.-_]+$", email))
+            {
+                EditText nameEntry = view.findViewById(R.id.editTextText2);
+                String name = nameEntry.getText().toString();
+
+                EditText surnameEntry = view.findViewById(R.id.editTextText3);
+                String surname = nameEntry.getText().toString();
+
+                if (name.equals("Ryszard") && surname.equals("Pyssa"))
+                {
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+
+                    fragmentTransaction.addToBackStack(null);
+                    fragmentTransaction.commit();
+                }
+            }
+        });
+        return view;
     }
 
 }
--- 2026_02_04/app/src/main/java/com/example/a2026_02_04/MainActivity.java
+++ 2026_02_04/app/src/main/java/com/example/a2026_02_04/MainActivity.java
@@ -2,49 +2,20 @@ package com.example.a2026_02_04;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-
-import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-
-//        Button button = (Button) findViewById(R.id.button);
-//        button.setOnClickListener(v -> {
-//            EditText emailEntry = (EditText) findViewById(R.id.editTextEmail);
-//            String email = emailEntry.getText().toString();
-//
-//            if(Pattern.matches("^[a-zA-Z0-9.-_]+@[a-zA-Z0-9.-_]+$", email))
-//            {
-//                EditText nameEntry = findViewById(R.id.editTextText2);
-//                String name = nameEntry.getText().toString();
-//
-//                EditText surnameEntry = findViewById(R.id.editTextText3);
-//                String surname = nameEntry.getText().toString();
-//
-//                if (name.equals("Ryszard") && surname.equals("Pyssa"))
-//                {
-//
-//                }
-//            }
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
 //        });
     }
 }
