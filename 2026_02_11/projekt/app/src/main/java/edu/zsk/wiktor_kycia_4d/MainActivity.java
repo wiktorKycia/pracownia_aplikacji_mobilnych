7c51b074373f3c5fd1543611766dacd27a2eb3d7
wiktor <wiktor.kycia@gmail.com>
2026-02-17 15:01:26 +0100

    add button onclick

--- 2026_02_11/projekt/app/src/main/java/edu/zsk/wiktor_kycia_4d/MainActivity.java
+++ 2026_02_11/projekt/app/src/main/java/edu/zsk/wiktor_kycia_4d/MainActivity.java
@@ -1,6 +1,12 @@
 package edu.zsk.wiktor_kycia_4d;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.app.ProgressDialog;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +21,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button button = findViewById(R.id.save_note_button);
+        button.setOnClickListener(v -> {
+            EditText name_input = findViewById(R.id.name_input);
+            EditText surname_input = findViewById(R.id.surname_input);
+            EditText class_input = findViewById(R.id.class_input);
+
+            if(name_input.getText().toString().isEmpty()
+            || surname_input.getText().toString().isEmpty()
+            || class_input.getText().toString().isEmpty())
+            {
+                Toast.makeText(this, "Wypełnij wszystkie pola!", Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                String name = name_input.getText().toString();
+                String surname = surname_input.getText().toString();
+                String class_ = class_input.getText().toString();
+
+                var i = new Intent();
+
+                var pd = new ProgressDialog("Dodaję uwagę");
+            }
         });
     }
 }
