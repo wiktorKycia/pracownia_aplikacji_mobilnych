2046f78a12610f8ac7e2cfb8ae851fad6d250af3
wiktor <wiktor.kycia@gmail.com>
2026-04-22 10:20:40 +0200

    change files

--- 2026_04_22/zadanie_Wiktor_Kycia/app/src/main/java/edu/zsk/kycia/LoggedInActivity.java
+++ 2026_04_22/zadanie_Wiktor_Kycia/app/src/main/java/edu/zsk/kycia/LoggedInActivity.java
@@ -1,14 +1,26 @@
 package edu.zsk.kycia;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class LoggedInActivity extends AppCompatActivity {
+
+    public final String CHANNEL_ID = "2137";
+    public final String CHANNEL_NAME = "Zadanie podsumowujące";
+
+    public String activeFragment;
+    public FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,24 @@ public class LoggedInActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        LinearLayout fragmentHolder = findViewById(R.id.fragmentHolder);
+
+        fragmentManager = new FragmentManager(){};
+        Fragment frag = fragmentManager.findFragmentById(R.id.fragment_first);
+
+        FragmentTransaction trans = fragmentManager.beginTransaction();
+        trans.add(fragmentHolder.getId(), frag, "fragment_first");
+        trans.commit();
+
+        Button changeFragmentButton = findViewById(R.id.button);
+        changeFragmentButton.setOnClickListener(v->{
+            changeFragment();
+        });
+
+    }
+    private void changeFragment()
+    {
+
     }
 }
--- 2026_04_22/zadanie_Wiktor_Kycia/app/src/main/java/edu/zsk/kycia/MainActivity.java
+++ 2026_04_22/zadanie_Wiktor_Kycia/app/src/main/java/edu/zsk/kycia/MainActivity.java
@@ -1,12 +1,17 @@
 package edu.zsk.kycia;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +25,40 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(v->{
+            EditText emailInput = findViewById(R.id.emailInput);
+            EditText passwordInput = findViewById(R.id.paswordInput);
+
+            String email = emailInput.getText().toString();
+            String password = passwordInput.getText().toString();
+
+            if(email.isEmpty() || password.isEmpty())
+            {
+                Toast.makeText(this, "Wypełnij wszystkie pola!", Toast.LENGTH_LONG).show();
+            }
+            else if(checkCredentials(email, password))
+            {
+                Toast.makeText(this, "Zalogowano!", Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                Toast.makeText(this, "Niepoprawne dane logowania!", Toast.LENGTH_LONG).show();
+                passwordInput.setText("");
+            }
+        });
+    }
+    private boolean checkCredentials(String email, String password)
+    {
+        if(Objects.equals(email, "admin@example.com") && Objects.equals(password, "admin"))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
